fa79c249741a878a2fdc7578cbb9bbe7788238a3
songlinhao <455674848@qq.com>
2018-07-05 16:58:41 +0800

    添加激活表。

--- sql/version1.sql
+++ sql/version1.sql
@@ -1,167 +1,245 @@
-drop table if exists account;
+DROP TABLE IF EXISTS account;
 
-create table account
+CREATE TABLE account
 (
-   id                   int not null auto_increment,
-   username             varchar(32) default NULL comment '用户名',
-   password             varchar(100) default NULL comment '密码',
-   phone                varchar(20) default NULL comment '手机号',
-   email                varchar(20) default NULL comment '邮箱',
-   union_id             varchar(50) default NULL comment '微信号',
-   last_login_time      timestamp default CURRENT_TIMESTAMP comment '最后登录时间',
-   create_time          timestamp default CURRENT_TIMESTAMP comment '创建时间',
-   modify_time          timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '编辑时间',
-   primary key (id)
+  id              INT NOT NULL AUTO_INCREMENT,
+  username        VARCHAR(32)  DEFAULT NULL
+  COMMENT '用户名',
+  password        VARCHAR(100) DEFAULT NULL
+  COMMENT '密码',
+  phone           VARCHAR(20)  DEFAULT NULL
+  COMMENT '手机号',
+  email           VARCHAR(20)  DEFAULT NULL
+  COMMENT '邮箱',
+  union_id        VARCHAR(50)  DEFAULT NULL
+  COMMENT '微信号',
+  last_login_time TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+  COMMENT '最后登录时间',
+  create_time     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+  COMMENT '创建时间',
+  modify_time     TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  COMMENT '编辑时间',
+  PRIMARY KEY (id)
 );
 
-alter table account comment '用户表';
+ALTER TABLE account
+  COMMENT '用户表';
 
-drop table if exists channel;
+DROP TABLE IF EXISTS channel;
 
-create table channel
+CREATE TABLE channel
 (
-   id                   int not null auto_increment,
-   uid                  int not null default 0 comment '用户id',
-   package_id           int not null default 0 comment '包id',
-   no                   varchar(50) not null comment '渠道编号',
-   name                 varchar(50) default NULL comment '渠道名',
-   type                 int not null default 0 comment '渠道类型',
-   stauts               int not null default 1 comment '状态 0关闭 1开启',
-   create_time          timestamp default CURRENT_TIMESTAMP,
-   modify_time          timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   primary key (id)
+  id          INT         NOT NULL AUTO_INCREMENT,
+  uid         INT         NOT NULL DEFAULT 0
+  COMMENT '用户id',
+  package_id  INT         NOT NULL DEFAULT 0
+  COMMENT '包id',
+  no          VARCHAR(50) NOT NULL
+  COMMENT '渠道编号',
+  name        VARCHAR(50)          DEFAULT NULL
+  COMMENT '渠道名',
+  type        INT         NOT NULL DEFAULT 0
+  COMMENT '渠道类型',
+  stauts      INT         NOT NULL DEFAULT 1
+  COMMENT '状态 0关闭 1开启',
+  create_time TIMESTAMP            DEFAULT CURRENT_TIMESTAMP,
+  modify_time TIMESTAMP            DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
 
-alter table channel comment '渠道';
+ALTER TABLE channel
+  COMMENT '渠道';
 
-drop table if exists real_time_data;
+DROP TABLE IF EXISTS real_time_data;
 
-create table real_time_data
+CREATE TABLE real_time_data
 (
-   id                   int not null auto_increment,
-   channel_no           varchar(50) not null comment '渠道编号',
-   pay_price            bigint not null default 0 comment '真实支付金额',
-   channel_price        bigint not null default 0 comment '渠道支付金额',
-   active               int not null default 0 comment '激活数',
-   pay                  int not null default 0 comment '支付数',
-   create_time          timestamp default CURRENT_TIMESTAMP,
-   modity_time          timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   primary key (id)
+  id            INT         NOT NULL AUTO_INCREMENT,
+  channel_no    VARCHAR(50) NOT NULL
+  COMMENT '渠道编号',
+  pay_price     BIGINT      NOT NULL DEFAULT 0
+  COMMENT '真实支付金额',
+  channel_price BIGINT      NOT NULL DEFAULT 0
+  COMMENT '渠道支付金额',
+  active        INT         NOT NULL DEFAULT 0
+  COMMENT '激活数',
+  pay           INT         NOT NULL DEFAULT 0
+  COMMENT '支付数',
+  create_time   TIMESTAMP            DEFAULT CURRENT_TIMESTAMP,
+  modity_time   TIMESTAMP            DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
 
-alter table real_time_data comment '实时统计表';
+ALTER TABLE real_time_data
+  COMMENT '实时统计表';
 
 
-drop table if exists banner;
+DROP TABLE IF EXISTS banner;
 
-create table banner
+CREATE TABLE banner
 (
-   id                   int not null auto_increment,
-   package_id           int not null default 0 comment '包id',
-   pic_url              varchar(200) default NULL comment '图片url',
-   url                  varchar(200) default NULL comment '跳转链接',
-   `desc`               varchar(200) default NULL comment '描述',
-   sort                 int not null default 9999 comment '排序',
-   status               int not null default 1 comment '状态 0未使用 1在使用',
-   create_time          timestamp default CURRENT_TIMESTAMP,
-   modify_time          timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   primary key (id)
+  id          INT NOT NULL AUTO_INCREMENT,
+  package_id  INT NOT NULL DEFAULT 0
+  COMMENT '包id',
+  pic_url     VARCHAR(200) DEFAULT NULL
+  COMMENT '图片url',
+  url         VARCHAR(200) DEFAULT NULL
+  COMMENT '跳转链接',
+  `desc`      VARCHAR(200) DEFAULT NULL
+  COMMENT '描述',
+  sort        INT NOT NULL DEFAULT 9999
+  COMMENT '排序',
+  status      INT NOT NULL DEFAULT 1
+  COMMENT '状态 0未使用 1在使用',
+  create_time TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
+  modify_time TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
 
-alter table banner comment '广告栏';
+ALTER TABLE banner
+  COMMENT '广告栏';
 
 
-drop table if exists package;
+DROP TABLE IF EXISTS package;
 
-create table package
+CREATE TABLE package
 (
-   id                   int not null auto_increment,
-   name                 varchar(100) default NULL comment '包名',
-   url                  varchar(200) default NULL comment '包下载地址',
-   pic                  varchar(200) default NULL comment '图片地址',
-   primary key (id)
+  id   INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) DEFAULT NULL
+  COMMENT '包名',
+  url  VARCHAR(200) DEFAULT NULL
+  COMMENT '包下载地址',
+  pic  VARCHAR(200) DEFAULT NULL
+  COMMENT '图片地址',
+  PRIMARY KEY (id)
 );
 
-alter table package comment '包信息';
+ALTER TABLE package
+  COMMENT '包信息';
 
 
-drop table if exists module;
+DROP TABLE IF EXISTS module;
 
-create table module
+CREATE TABLE module
 (
-   id                   int not null auto_increment,
-   package_id           int not null default 0 comment '包id',
-   name                 varchar(50) default NULL comment '模块名',
-   icon                 varchar(200) default NULL comment 'icon',
-   primary key (id)
+  id         INT NOT NULL AUTO_INCREMENT,
+  package_id INT NOT NULL DEFAULT 0
+  COMMENT '包id',
+  name       VARCHAR(50)  DEFAULT NULL
+  COMMENT '模块名',
+  icon       VARCHAR(200) DEFAULT NULL
+  COMMENT 'icon',
+  PRIMARY KEY (id)
 );
 
-alter table module comment '模块';
+ALTER TABLE module
+  COMMENT '模块';
 
 
-drop table if exists catalog;
+DROP TABLE IF EXISTS catalog;
 
-create table catalog
+CREATE TABLE catalog
 (
-   id                   int not null auto_increment,
-   package_id           int default 0 comment '包id',
-   name                 varchar(50) default NULL comment '分类名',
-   url                  varchar(200) default NULL comment '分类链接',
-   sort                 int not null default 9999 comment '排名',
-   primary key (id)
+  id         INT NOT NULL AUTO_INCREMENT,
+  package_id INT          DEFAULT 0
+  COMMENT '包id',
+  name       VARCHAR(50)  DEFAULT NULL
+  COMMENT '分类名',
+  url        VARCHAR(200) DEFAULT NULL
+  COMMENT '分类链接',
+  sort       INT NOT NULL DEFAULT 9999
+  COMMENT '排名',
+  PRIMARY KEY (id)
 );
 
-alter table catalog comment '分类';
+ALTER TABLE catalog
+  COMMENT '分类';
 
 
-drop table if exists resource;
+DROP TABLE IF EXISTS resource;
 
-create table resource
+CREATE TABLE resource
 (
-   id                   int not null auto_increment,
-   catalog_id           int default 0 comment '分类id',
-   type                 int default 0 comment '类型 1图片 2视频',
-   `desc`               varchar(500) default NULL comment '描述',
-   name                 varchar(200) default NULL comment '名',
-   url                  varchar(200) default NULL comment '列表图片地址',
-   statistic            int default 0 comment '资源点击次数',
-   create_time          timestamp default CURRENT_TIMESTAMP,
-   modify_time          timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   primary key (id)
+  id          INT NOT NULL AUTO_INCREMENT,
+  catalog_id  INT          DEFAULT 0
+  COMMENT '分类id',
+  type        INT          DEFAULT 0
+  COMMENT '类型 1图片 2视频',
+  `desc`      VARCHAR(500) DEFAULT NULL
+  COMMENT '描述',
+  name        VARCHAR(200) DEFAULT NULL
+  COMMENT '名',
+  url         VARCHAR(200) DEFAULT NULL
+  COMMENT '列表图片地址',
+  statistic   INT          DEFAULT 0
+  COMMENT '资源点击次数',
+  create_time TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
+  modify_time TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
 
-alter table resource comment '资源';
+ALTER TABLE resource
+  COMMENT '资源';
 
 
-drop table if exists resource_info;
+DROP TABLE IF EXISTS resource_info;
 
-create table resource_info
+CREATE TABLE resource_info
 (
-   id                   int not null auto_increment,
-   resource_id          int default 0 comment '资源id',
-   url                  varchar(200) default NULL comment '地址',
-   sort                 int default 9999 comment '排序',
-   create_time          timestamp default CURRENT_TIMESTAMP,
-   modify_time          timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   primary key (id)
+  id          INT NOT NULL AUTO_INCREMENT,
+  resource_id INT          DEFAULT 0
+  COMMENT '资源id',
+  url         VARCHAR(200) DEFAULT NULL
+  COMMENT '地址',
+  sort        INT          DEFAULT 9999
+  COMMENT '排序',
+  create_time TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
+  modify_time TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
 
-alter table resource_info comment '资源详细';
+ALTER TABLE resource_info
+  COMMENT '资源详细';
 
-drop table if exists video_user;
+DROP TABLE IF EXISTS video_user;
 
-create table video_user
+CREATE TABLE video_user
 (
-   id                   int not null auto_increment,
-   channel_no           varchar(50) default NULL comment '渠道编号',
-   type                 int default 1 comment '类型 1:普通 2:vip 3:高级vip',
-   cell_model           varchar(50) default NULL comment '手机型号',
-   cell_version         varchar(50) default NULL comment '系统版本',
-   package_id           int default 0 comment '包id',
-   create_time          timestamp default CURRENT_TIMESTAMP,
-   modify_time          timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   primary key (id)
+  id           INT NOT NULL AUTO_INCREMENT,
+  channel_no   VARCHAR(50)  DEFAULT NULL
+  COMMENT '渠道编号',
+  type         INT          DEFAULT 1
+  COMMENT '类型 1:普通 2:vip 3:高级vip',
+  cell_model   VARCHAR(50)  DEFAULT NULL
+  COMMENT '手机型号',
+  cell_version VARCHAR(50)  DEFAULT NULL
+  COMMENT '系统版本',
+  package_id   INT          DEFAULT 0
+  COMMENT '包id',
+  create_time  TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
+  modify_time  TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
 );
 
-alter table video_user comment '视频用户';
+ALTER TABLE video_user
+  COMMENT '视频用户';
+
+DROP TABLE IF EXISTS video_user_active;
+
+CREATE TABLE video_user_active
+(
+  id            INT NOT NULL AUTO_INCREMENT,
+  code          VARCHAR(20)  DEFAULT NULL
+  COMMENT '激活码',
+  video_user_id INT          DEFAULT 0
+  COMMENT '视频用户id',
+  status        INT          DEFAULT 0
+  COMMENT '状态 0:未使用 1:已使用',
+  create_time   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
+  modify_time   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE video_user_active
+  COMMENT '激活码';
 
